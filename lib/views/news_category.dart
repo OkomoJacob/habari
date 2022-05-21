f1372fece925962dc5a9cd5d8cf47323c3ef1208
okomojacob <jaylabs2030@gmail.com>
2022-05-21 18:19:05 +0300

    Initial revamps :pencil:

---
+++ lib/views/news_category.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewsCaregory extends StatefulWidget {
+  const NewsCaregory({ Key? key }) : super(key: key);
+
+  @override
+  State<NewsCaregory> createState() => _NewsCaregoryState();
+}
+
+class _NewsCaregoryState extends State<NewsCaregory> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
