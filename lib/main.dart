f30211fd7f4b13b278bbb5429485d5ab085141a6
okomojacob <jaylabs2030@gmail.com>
2022-05-27 07:40:57 +0300

    Primary color :bug:

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:habari/views/home_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -14,9 +11,9 @@ class MyApp extends StatelessWidget {
       title: 'Habari App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
       ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
