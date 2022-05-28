4184b8f018319b8e8aea98351d639e61996efe6a
okomojacob <jaylabs2030@gmail.com>
2022-05-28 15:04:13 +0300

    Categories section :books:

--- lib/helpers/news.dart
+++ lib/helpers/news.dart
@@ -31,3 +31,31 @@ class News {
     }
   }
 }
+class CategoryNewsClass {
+  List<ArticleModel> news = [];
+
+  Future<void> getNews(String category) async {
+    String url = "https://newsapi.org/v2/top-headlines?category=$category&country=us&category=business&apiKey=aaeb65a65b73483981f0c720e9d48d5a";
+    var response = await http.get(Uri.parse(url));
+    var jsonData = jsonDecode(response.body);
+    // Json API request status code == 200?
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        // Only display articles with both image and descriptions
+        if (element["urlToImage"] != null && element['description'] != null) {
+          ArticleModel articleModel = ArticleModel(
+              title: element['title'],
+              author: element['author'],
+              description: element['description'],
+              url: element["url"],
+              urlToImage: element["urlToImage"],
+              // publishedAt: element["publishedAt"],
+              content: element["content"]);
+
+          // Update the newsList with the updated article model
+          news.add(articleModel);
+        }
+      });
+    }
+  }
+}
