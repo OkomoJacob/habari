4432a0642d59c21df33996e2294aeb6c9db4985d
okomojacob <jaylabs2030@gmail.com>
2022-05-28 12:06:59 +0300

    Fixed :hammer: business card :bug:

--- lib/helpers/data.dart
+++ lib/helpers/data.dart
@@ -5,9 +5,9 @@ List<CategoryModel> getCategories() {
   CategoryModel categoryModel = CategoryModel();
 
   // 1. Business
+  categoryModel = CategoryModel();
   categoryModel.categoryName = "Business";
-  categoryModel.imageUrl =
-      "https://images.unsplash.com/photo-1520607162513-77705c0f0d4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YnVzaW5lc3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60";
   // Add categoy model to list
   category.add(categoryModel);
   
