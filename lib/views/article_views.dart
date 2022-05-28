383fe4048d5211efc418613085362eb4659014a0
okomojacob <jaylabs2030@gmail.com>
2022-05-28 15:04:53 +0300

    Completer, controller for webview

--- lib/views/article_views.dart
+++ lib/views/article_views.dart
@@ -1,15 +1,62 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticlesViews extends StatefulWidget {
-  const ArticlesViews({Key? key}) : super(key: key);
+  final String blogUrl;
+  ArticlesViews({required this.blogUrl});
 
   @override
   State<ArticlesViews> createState() => _ArticlesViewsState();
 }
 
 class _ArticlesViewsState extends State<ArticlesViews> {
+  // Why completer and controllers?
+  final Completer<WebViewController> _completer =
+      Completer<WebViewController>();
+
+  void initState() {
+     super.initState();
+     // Enable virtual display.
+     if (Platform.isAndroid) WebView.platform = AndroidWebView();
+   }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text("Flutter"),
+            Text(
+              "News",
+              style: TextStyle(color: Colors.redAccent),
+            )
+          ],
+        ),
+        actions: [
+          Opacity(
+            opacity: 0,
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Icon(Icons.safety_check)),
+          )
+        ],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: WebView(
+          initialUrl: widget.blogUrl,
+          onWebViewCreated: ((WebViewController webViewController) {
+            _completer.complete(webViewController);
+          }),
+        ),
+      ),
+    );
   }
 }
